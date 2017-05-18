b59e5d116aca594a61bac2ef30ec6f44f118b820
Jackie <jackiechoi66@gmail.com>
2017-05-18 14:06:44 +0200

    Adding routes.js with nested children components to App.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-//Router:
-//brownserHistory: 
+//brownserHistory: when URL updates, browserHistory updates everything after the protocol (http)
 import { Router, browserHistory } from 'react-router';
-
-
 import reducers from './reducers';
+import routes from './routes'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-  	<Router history={browserHistory} /> 
+  	<Router history={browserHistory} routes={routes}/> 
   </Provider>
   , document.querySelector('.container'));
